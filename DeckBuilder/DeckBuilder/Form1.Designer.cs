54fdf917b75cd22f55fd138c8a77d5a1b518ed19
daydream89 <nodoka89@naver.com>
2019-04-15 18:37:29 +0900

    add card image print
    판 별로 관리할 수 있는 eExpansion enum 추가.
    카드 이미지 출력 기능 추가

--- DeckBuilder/DeckBuilder/Form1.Designer.cs
+++ DeckBuilder/DeckBuilder/Form1.Designer.cs
@@ -30,18 +30,25 @@
 		{
 			this.CardDatabaseGB = new System.Windows.Forms.GroupBox();
 			this.ListCardImgGB = new System.Windows.Forms.GroupBox();
+			this.ListCardImg = new System.Windows.Forms.PictureBox();
 			this.CardOptionGB = new System.Windows.Forms.GroupBox();
+			this.ExpansionComboBox = new System.Windows.Forms.ComboBox();
 			this.CardDataProgressBar = new System.Windows.Forms.ProgressBar();
 			this.RefreshListBtn = new System.Windows.Forms.Button();
 			this.CrawlingCardBtn = new System.Windows.Forms.Button();
 			this.CardListBox = new System.Windows.Forms.ListBox();
 			this.DeckBuilderGB = new System.Windows.Forms.GroupBox();
 			this.DeckCardImgGB = new System.Windows.Forms.GroupBox();
+			this.DeckCardImg = new System.Windows.Forms.PictureBox();
 			this.DeckOptionGB = new System.Windows.Forms.GroupBox();
 			this.DeckList = new System.Windows.Forms.ListBox();
 			this.CardDatabaseGB.SuspendLayout();
+			this.ListCardImgGB.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ListCardImg)).BeginInit();
 			this.CardOptionGB.SuspendLayout();
 			this.DeckBuilderGB.SuspendLayout();
+			this.DeckCardImgGB.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.DeckCardImg)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// CardDatabaseGB
@@ -51,54 +58,74 @@
 			this.CardDatabaseGB.Controls.Add(this.CardListBox);
 			this.CardDatabaseGB.Location = new System.Drawing.Point(12, 12);
 			this.CardDatabaseGB.Name = "CardDatabaseGB";
-			this.CardDatabaseGB.Size = new System.Drawing.Size(380, 426);
+			this.CardDatabaseGB.Size = new System.Drawing.Size(441, 530);
 			this.CardDatabaseGB.TabIndex = 0;
 			this.CardDatabaseGB.TabStop = false;
 			this.CardDatabaseGB.Text = "Card Database";
 			// 
 			// ListCardImgGB
 			// 
-			this.ListCardImgGB.Location = new System.Drawing.Point(192, 136);
+			this.ListCardImgGB.Controls.Add(this.ListCardImg);
+			this.ListCardImgGB.Location = new System.Drawing.Point(192, 175);
 			this.ListCardImgGB.Name = "ListCardImgGB";
-			this.ListCardImgGB.Size = new System.Drawing.Size(182, 282);
+			this.ListCardImgGB.Size = new System.Drawing.Size(238, 349);
 			this.ListCardImgGB.TabIndex = 2;
 			this.ListCardImgGB.TabStop = false;
 			this.ListCardImgGB.Text = "Card Image";
 			// 
+			// ListCardImg
+			// 
+			this.ListCardImg.Location = new System.Drawing.Point(7, 25);
+			this.ListCardImg.Margin = new System.Windows.Forms.Padding(0);
+			this.ListCardImg.Name = "ListCardImg";
+			this.ListCardImg.Size = new System.Drawing.Size(223, 321);
+			this.ListCardImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ListCardImg.TabIndex = 0;
+			this.ListCardImg.TabStop = false;
+			// 
 			// CardOptionGB
 			// 
+			this.CardOptionGB.Controls.Add(this.ExpansionComboBox);
 			this.CardOptionGB.Controls.Add(this.CardDataProgressBar);
 			this.CardOptionGB.Controls.Add(this.RefreshListBtn);
 			this.CardOptionGB.Controls.Add(this.CrawlingCardBtn);
 			this.CardOptionGB.Location = new System.Drawing.Point(192, 24);
 			this.CardOptionGB.Name = "CardOptionGB";
-			this.CardOptionGB.Size = new System.Drawing.Size(182, 106);
+			this.CardOptionGB.Size = new System.Drawing.Size(238, 145);
 			this.CardOptionGB.TabIndex = 1;
 			this.CardOptionGB.TabStop = false;
 			this.CardOptionGB.Text = "Option";
 			// 
+			// ExpansionComboBox
+			// 
+			this.ExpansionComboBox.FormattingEnabled = true;
+			this.ExpansionComboBox.Location = new System.Drawing.Point(7, 48);
+			this.ExpansionComboBox.Name = "ExpansionComboBox";
+			this.ExpansionComboBox.Size = new System.Drawing.Size(223, 23);
+			this.ExpansionComboBox.TabIndex = 3;
+			// 
 			// CardDataProgressBar
 			// 
-			this.CardDataProgressBar.Location = new System.Drawing.Point(6, 63);
+			this.CardDataProgressBar.Location = new System.Drawing.Point(6, 116);
 			this.CardDataProgressBar.Name = "CardDataProgressBar";
-			this.CardDataProgressBar.Size = new System.Drawing.Size(168, 23);
+			this.CardDataProgressBar.Size = new System.Drawing.Size(224, 23);
 			this.CardDataProgressBar.TabIndex = 2;
 			// 
 			// RefreshListBtn
 			// 
 			this.RefreshListBtn.Font = new System.Drawing.Font("굴림", 8F);
-			this.RefreshListBtn.Location = new System.Drawing.Point(129, 24);
+			this.RefreshListBtn.Location = new System.Drawing.Point(130, 77);
 			this.RefreshListBtn.Name = "RefreshListBtn";
-			this.RefreshListBtn.Size = new System.Drawing.Size(46, 33);
+			this.RefreshListBtn.Size = new System.Drawing.Size(102, 33);
 			this.RefreshListBtn.TabIndex = 1;
-			this.RefreshListBtn.Text = "갱신";
+			this.RefreshListBtn.Text = "리스트 갱신";
 			this.RefreshListBtn.UseVisualStyleBackColor = true;
 			this.RefreshListBtn.Click += new System.EventHandler(this.RefreshListBtn_Click);
 			// 
 			// CrawlingCardBtn
 			// 
 			this.CrawlingCardBtn.Font = new System.Drawing.Font("굴림", 8F);
-			this.CrawlingCardBtn.Location = new System.Drawing.Point(6, 24);
+			this.CrawlingCardBtn.Location = new System.Drawing.Point(7, 77);
 			this.CrawlingCardBtn.Name = "CrawlingCardBtn";
 			this.CrawlingCardBtn.Size = new System.Drawing.Size(117, 33);
 			this.CrawlingCardBtn.TabIndex = 0;
@@ -112,35 +139,45 @@
 			this.CardListBox.ItemHeight = 15;
 			this.CardListBox.Location = new System.Drawing.Point(6, 24);
 			this.CardListBox.Name = "CardListBox";
-			this.CardListBox.Size = new System.Drawing.Size(180, 394);
+			this.CardListBox.Size = new System.Drawing.Size(180, 499);
 			this.CardListBox.TabIndex = 0;
+			this.CardListBox.SelectedIndexChanged += new System.EventHandler(this.CardListBox_ItemSelected);
 			// 
 			// DeckBuilderGB
 			// 
 			this.DeckBuilderGB.Controls.Add(this.DeckCardImgGB);
 			this.DeckBuilderGB.Controls.Add(this.DeckOptionGB);
 			this.DeckBuilderGB.Controls.Add(this.DeckList);
-			this.DeckBuilderGB.Location = new System.Drawing.Point(409, 12);
+			this.DeckBuilderGB.Location = new System.Drawing.Point(459, 12);
 			this.DeckBuilderGB.Name = "DeckBuilderGB";
-			this.DeckBuilderGB.Size = new System.Drawing.Size(380, 426);
+			this.DeckBuilderGB.Size = new System.Drawing.Size(439, 530);
 			this.DeckBuilderGB.TabIndex = 1;
 			this.DeckBuilderGB.TabStop = false;
 			this.DeckBuilderGB.Text = "Deck Builder";
 			// 
 			// DeckCardImgGB
 			// 
-			this.DeckCardImgGB.Location = new System.Drawing.Point(192, 136);
+			this.DeckCardImgGB.Controls.Add(this.DeckCardImg);
+			this.DeckCardImgGB.Location = new System.Drawing.Point(192, 174);
 			this.DeckCardImgGB.Name = "DeckCardImgGB";
-			this.DeckCardImgGB.Size = new System.Drawing.Size(182, 282);
+			this.DeckCardImgGB.Size = new System.Drawing.Size(238, 350);
 			this.DeckCardImgGB.TabIndex = 2;
 			this.DeckCardImgGB.TabStop = false;
 			this.DeckCardImgGB.Text = "Card Image";
+			// 
+			// DeckCardImg
+			// 
+			this.DeckCardImg.Location = new System.Drawing.Point(7, 25);
+			this.DeckCardImg.Name = "DeckCardImg";
+			this.DeckCardImg.Size = new System.Drawing.Size(223, 324);
+			this.DeckCardImg.TabIndex = 0;
+			this.DeckCardImg.TabStop = false;
 			// 
 			// DeckOptionGB
 			// 
 			this.DeckOptionGB.Location = new System.Drawing.Point(192, 24);
 			this.DeckOptionGB.Name = "DeckOptionGB";
-			this.DeckOptionGB.Size = new System.Drawing.Size(182, 106);
+			this.DeckOptionGB.Size = new System.Drawing.Size(238, 144);
 			this.DeckOptionGB.TabIndex = 1;
 			this.DeckOptionGB.TabStop = false;
 			this.DeckOptionGB.Text = "Option";
@@ -151,21 +188,25 @@
 			this.DeckList.ItemHeight = 15;
 			this.DeckList.Location = new System.Drawing.Point(6, 24);
 			this.DeckList.Name = "DeckList";
-			this.DeckList.Size = new System.Drawing.Size(180, 394);
+			this.DeckList.Size = new System.Drawing.Size(180, 499);
 			this.DeckList.TabIndex = 0;
 			// 
 			// DeckBuilder
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(907, 554);
 			this.Controls.Add(this.DeckBuilderGB);
 			this.Controls.Add(this.CardDatabaseGB);
 			this.Name = "DeckBuilder";
 			this.Text = "MTG Deck Builder";
 			this.CardDatabaseGB.ResumeLayout(false);
+			this.ListCardImgGB.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.ListCardImg)).EndInit();
 			this.CardOptionGB.ResumeLayout(false);
 			this.DeckBuilderGB.ResumeLayout(false);
+			this.DeckCardImgGB.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.DeckCardImg)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -183,6 +224,9 @@
 		private System.Windows.Forms.Button CrawlingCardBtn;
 		private System.Windows.Forms.Button RefreshListBtn;
 		private System.Windows.Forms.ProgressBar CardDataProgressBar;
+		private System.Windows.Forms.PictureBox ListCardImg;
+		private System.Windows.Forms.PictureBox DeckCardImg;
+		private System.Windows.Forms.ComboBox ExpansionComboBox;
 	}
 }
 
--- DeckBuilder/DeckBuilder/Form1.cs
+++ DeckBuilder/DeckBuilder/Form1.cs
@@ -11,11 +11,23 @@ using System.Windows.Forms;
 
 namespace DeckBuilder
 {
+	enum eExpansion
+	{
+		IXALAN = 0,
+		RIVALS_OF_IXALAN,
+		DOMINARIA,
+		CORE_SET_2019,
+		GUILD_OF_RAVNICA,
+		RAVNICA_ALLEGIANCE,
+
+		EXPANSION_MAX,
+	}
+
 	public partial class DeckBuilder : Form
 	{
 		private const String m_cardDataDir = "CardData\\";
-		private Dictionary<String, int> m_setStartIdList;
-		private Dictionary<String, int> m_cardSetNumList;
+		private Dictionary<eExpansion, int> m_setStartIdList;
+		private Dictionary<eExpansion, int> m_cardSetNumList;
 
 		private WebLibrary m_WebLibrary;
 		private Dictionary<String, CardData> m_CardList;
@@ -25,23 +37,79 @@ namespace DeckBuilder
 			InitializeComponent();
 
 			// need change this file read later.
-			m_setStartIdList = new Dictionary<String, int>{ { "ixalan", 436647 }, { "rivals of ixalan", 440876 }, { "dominaria", 444503 },
-														   { "core set 2019", 448823 }, { "guild of ravnica", 454305 }, { "ravnica allegiance", 458699 } };
+			m_setStartIdList = new Dictionary<eExpansion, int>{ { eExpansion.IXALAN, 436647 }, { eExpansion.RIVALS_OF_IXALAN, 440876 }, { eExpansion.DOMINARIA, 444503 },
+														   { eExpansion.CORE_SET_2019, 448823 }, { eExpansion.GUILD_OF_RAVNICA, 454305 }, { eExpansion.RAVNICA_ALLEGIANCE, 458699 } };
 
-			m_cardSetNumList = new Dictionary<String, int>{ { "ixalan", 284 }, { "rivals of ixalan", 212 }, { "dominaria", 265 },
-														    { "core set 2019", 280 }, { "guild of ravnica", 288 }, { "ravnica allegiance", 270 } };
+			m_cardSetNumList = new Dictionary<eExpansion, int>{ { eExpansion.IXALAN, 284 }, { eExpansion.RIVALS_OF_IXALAN, 212 }, { eExpansion.DOMINARIA, 265 },
+														    { eExpansion.CORE_SET_2019, 280 }, { eExpansion.GUILD_OF_RAVNICA, 288 }, { eExpansion.RAVNICA_ALLEGIANCE, 270 } };
 
 			if (Directory.Exists(m_cardDataDir) == false)
 				Directory.CreateDirectory(m_cardDataDir);
 
 			m_CardList = new Dictionary<String, CardData>();
 			m_WebLibrary = new WebLibrary();
+
+			//for test
+			/*String name = "가시 장교";
+			CardData data = new CardData();
+			data.SetImagePath("CardData\\Images\\가시 장교.jpeg");
+			m_CardList.Add(name, data);
+			CardListBox.Items.Add(name);*/
+
+			SetExpansionComboBox();
+		}
+
+		private String GetStringFromExpansionEnum(eExpansion expansion)
+		{
+			switch (expansion)
+			{
+				case eExpansion.IXALAN: return "Ixalan";
+				case eExpansion.RIVALS_OF_IXALAN: return "Rivals of Ixalan";
+				case eExpansion.DOMINARIA: return "Dominaria";
+				case eExpansion.CORE_SET_2019: return "Core Set 2019";
+				case eExpansion.GUILD_OF_RAVNICA: return "Guild of Ravnica";
+				case eExpansion.RAVNICA_ALLEGIANCE: return "Ravnica Allegiance";
+				default: return "";
+			}
+		}
+
+		private eExpansion GetExpansionEnumFromString(String expansionName)
+		{
+			if (expansionName == "Ixalan")
+				return eExpansion.IXALAN;
+			else if (expansionName == "Rivals of Ixalan")
+				return eExpansion.RIVALS_OF_IXALAN;
+			else if (expansionName == "Dominaria")
+				return eExpansion.DOMINARIA;
+			else if (expansionName == "Core Set 2019")
+				return eExpansion.CORE_SET_2019;
+			else if (expansionName == "Guild of Ravnica")
+				return eExpansion.GUILD_OF_RAVNICA;
+			else if (expansionName == "Ravnica Allegiance")
+				return eExpansion.RAVNICA_ALLEGIANCE;
+
+			return eExpansion.EXPANSION_MAX;
+		}
+
+		public void SetExpansionComboBox()
+		{
+			ExpansionComboBox.Items.Add("Ixalan");
+			ExpansionComboBox.Items.Add("Rivals of Ixalan");
+			ExpansionComboBox.Items.Add("Dominaria");
+			ExpansionComboBox.Items.Add("Core Set 2019");
+			ExpansionComboBox.Items.Add("Guild of Ravnica");
+			ExpansionComboBox.Items.Add("Ravnica Allegiance");
+
+			ExpansionComboBox.SelectedIndex = 0;
 		}
 
 		public void CrawlingCard()
 		{
-			int cardNum = m_cardSetNumList["core set 2019"];
-			int startCardId = m_setStartIdList["core set 2019"];
+			String expansionName = ExpansionComboBox.SelectedText;
+			eExpansion expansion = GetExpansionEnumFromString(expansionName);
+
+			int cardNum = m_cardSetNumList[expansion];
+			int startCardId = m_setStartIdList[expansion];
 			for (int i = 0; i < cardNum; ++i)
 			{
 				int cardID = startCardId + i;
@@ -54,7 +122,7 @@ namespace DeckBuilder
 				if (m_CardList.ContainsKey(card.GetCardName()) == false)
 					m_CardList.Add(card.GetCardName(), card);
 
-				double progressRate = (double)m_CardList.Count / (double)m_cardSetNumList["core set 2019"];
+				double progressRate = (double)m_CardList.Count / (double)m_cardSetNumList[expansion];
 				CardDataProgressBar.Value = (int)(progressRate * 100);
 			}
 		}
@@ -70,6 +138,13 @@ namespace DeckBuilder
 			{
 				CardListBox.Items.Add(card.Key);
 			}
+		}
+
+		private void CardListBox_ItemSelected(object sender, EventArgs e)
+		{
+			// C#에서 casting 방법이 있을듯?
+			String name = CardListBox.SelectedItem as String;
+			ListCardImg.Image = Image.FromFile(m_CardList[name].GetImagePath());
 		}
 	}
 }
